b4d83276c1aa8fc3e2fbef92347d2a3a62f188d5
MOSALAHDEV <mosalah525446@gmail.com>
2025-06-14 00:30:14 +0000

    Add python script that determine the fewest number of coins needed to meet a given amount total.

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+0-making_change.py
+Defines makeChange(coins, total) that returns the minimum number of coins
+needed to make up the given total with infinite supply of each coin.
+"""
+
+def makeChange(coins, total):
+    """
+    coins: list of positive integers (denominations available)
+    total: non-negative integer target sum
+
+    Returns:
+    - 0 if total <= 0
+    - minimum number of coins to make `total`, or
+    - -1 if total cannot be reached
+    """
+    if total <= 0:
+        return 0
+    
+    coins.sort(reverse=True)
+    
+    count = 0
+    for coin in coins:
+        while total >= coin:
+            total -= coin
+            count += 1
+    
+    if total == 0:
+        return count
+    else:
+        return -1
