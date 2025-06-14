e37d0b99fd83bf0f99626ae97b9deaf45bd46e82
MOSALAHDEV <mosalah525446@gmail.com>
2025-06-14 00:45:38 +0000

    Fix: validated pycodestyle

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,6 +5,7 @@ Defines makeChange(coins, total) that returns the minimum number of coins
 needed to make up the given total with infinite supply of each coin.
 """
 
+
 def makeChange(coins, total):
     """
     coins: list of positive integers (denominations available)
@@ -17,15 +18,15 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    
+
     coins.sort(reverse=True)
-    
+
     count = 0
     for coin in coins:
         while total >= coin:
             total -= coin
             count += 1
-    
+
     if total == 0:
         return count
     else:
